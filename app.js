c005c53f0bbe7c0ff63ba7b92ae7ca8e324e03c4
ahmed <elshehry@gamil.com>
2023-10-13 17:43:42 +0300

    Added security headers using helmet

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,6 +9,10 @@ const toursRouter = require('./routes/toursRoutes');
 const usersRoutes = require('./routes/usersRoutes');
 
 const app = express();
+
+//Set security http headers
+app.use(helmet());
+
 
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
@@ -20,13 +25,18 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 
+//Limit data incoming from the request body
+app.use(express.json({ limit: '10kb' }));
+
+app.use((req, res, next) => {
+  req.requestedAt = new Date().toISOString();
+  next();
+});
+
+//Allowig access to the static files
+app.use(express.static(`${__dirname}/public`));
+
 app
-  .use(express.json())
-  .use((req, res, next) => {
-    req.requestedAt = new Date().toISOString();
-    next();
-  })
-  .use(express.static(`${__dirname}/public`))
   .use('/api/v1/tours', toursRouter)
   .use('/api/v1/users', usersRoutes);
 
@@ -35,6 +45,7 @@ app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
+//Using global error handling middleware
 app.use(globalErrorHandler);
 
 module.exports = app;
