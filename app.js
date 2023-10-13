74187f0b424ebb00813b22f06dfa5156e70821bb
ahmed <elshehry@gamil.com>
2023-10-13 17:32:14 +0300

    Limited the number of requests coming from certain IP address

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -9,6 +10,15 @@ const usersRoutes = require('./routes/usersRoutes');
 const app = express();
 
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+//Limiting num.of requests for certain IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from that IP, try again within an hour!'
+});
+
+app.use('/api', limiter);
 
 app
   .use(express.json())
