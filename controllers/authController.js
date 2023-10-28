857628bdf2ae046ec824a877ee8997386d741b0b
ahmed <elshehry@gamil.com>
2023-10-28 22:16:20 +0200

    Fixed the auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -34,7 +34,7 @@ const createAndSendTokens = (user, res, statusCode) => {
                     : process.env.REFRESHH_TOKEN_COOKIE_EXPIRES_IN}`
                 * 24 * 60 * 60 * 1000),
             httpOnly: true,
-            secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
+            secure: req.secure
         };
     };
 
