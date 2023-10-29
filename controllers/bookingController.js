c326abfd4662532039d4d33e0efa04608c572a07
ahmed <elshehry@gamil.com>
2023-10-29 15:29:01 +0200

    Fixed the webhook checkout

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -54,7 +54,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async session => {
     const tour = session.client_reference_id;
     const user = (await User.findOne({ email: session.customer_email }))._id;
-    const price = session.line_items[0].price_data.unit_amount / 100;
+    const price = session.display_items[0].price_data.unit_amount / 100;
 
     await Booking.create({ tour, user, price });
 };
@@ -75,7 +75,7 @@ exports.webhookCheckout = (req, res, next) => {
         return res.status(400).send(`Webhook error: ${err.message}`);
     }
 
-    if (event.type === 'checkout.session.complete') createBookingCheckout(event.data.object);
+    if (event.type === 'checkout.session.completed') createBookingCheckout(event.data.object);
 
     res.status(200).json({
         recieved: true
